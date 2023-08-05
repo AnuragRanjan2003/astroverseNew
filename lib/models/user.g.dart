97384c5d403da59de25768661c6ef82322dfdeff
AnuragRanjan2003 <anuragranjan1261@gmail.com>
2023-08-06 02:16:31 +0530

    auth on the way

---
+++ lib/models/user.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      json['name'] as String,
+      json['email'] as String,
+      json['image'] as String,
+      json['plan'] as int,
+      json['uid'] as String,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+      'image': instance.image,
+      'uid': instance.uid,
+      'plan': instance.plan,
+    };
